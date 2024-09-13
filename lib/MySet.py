919f498c774a033de319ac5538a683f17d342319
adanjelle <adanjelle4@gmail.com>
2024-09-13 12:07:35 +0300

    complete

--- lib/MySet.py
+++ lib/MySet.py
@@ -1,3 +1,18 @@
 class MySet:
+    def __init__(self, enumerable=None):
+        self.dictionary = {}
+        if enumerable is None:
+            enumerable = []
+        for value in enumerable:
+            self.dictionary[value] = True
 
-    pass
+    # Method to check if a value exists in the set
+    def has(self, value):
+        return value in self.dictionary
+
+    # Method to add a value to the set
+    def add(self, value):
+        self.dictionary[value] = True
+    def delete(self, value):
+     self.dictionary.pop(value, None)
+    
